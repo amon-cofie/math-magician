a090d73b0680a23d69f0f838fcba716063f2bebd
amon-cofie <ddd.amon123@gmail.com>
2022-11-03 10:00:36 +0000

    build full website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,8 +13,23 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Calculator />
+      <div className="background-style">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route
+            path="/calculator"
+            element={(
+              <>
+                <div className="App">
+                  <Calculator />
+                  <div className="tag-line">Let&apos;s do some Math!</div>
+                </div>
+              </>
+            )}
+          />
+        </Routes>
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: uuidv4(),
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: uuidv4(),
+      path: '/calculator',
+      text: 'Calculator',
+    },
+    {
+      id: uuidv4(),
+      path: '/quote',
+      text: 'Quote',
+    },
+  ];
+
+  return (
+    <nav className="navbar-container">
+      <h1 className="main-heading">Math Magicians</h1>
+      <ul className="nav-links">
+        {links.map((link) => (
+          <li key={link.id}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
