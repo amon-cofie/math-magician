cb7badf3792aeb8de45685360a30786e67806a40
nahnahAJ <nahnahahjuwah@gmail.com>
2022-11-04 12:26:50 +0000

    Added tests for components

---
+++ src/__tests__/Components.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Home from '../pages/Home';
+import Quote from '../pages/Quote';
+
+
+test('renders navbar component snapshot', () => {
+  const { asFragment } = render(
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>,
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('renders Home component snapshot', () => {
+  const { asFragment } = render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>,
+  );
+  expect(asFragment()).toMatchSnapshot();
+})
+
+test('renders About component snapshot', () => {
+  const { asFragment } = render(
+    <BrowserRouter>
+      <Quote />
+    </BrowserRouter>,
+  );
+  expect(asFragment()).toMatchSnapshot();
+})
